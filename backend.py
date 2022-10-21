ceeba79db6c0cb2842017aa3e8606824829386c4
Bhooyas <bak05102000@gmail.com>
2022-10-21 18:59:10 +0530

    Updated Cros

--- backend.py
+++ backend.py
@@ -8,7 +8,10 @@ from PIL import Image
 import io
 
 app = Flask(__name__)
-CORS(app)
+api_v1_cors_config = {
+  "origins": ["*"]
+}
+CORS(app, resources={"/api/v1/*": api_v1_cors_config})
 
 interpreter = tf.lite.Interpreter(model_path="converted_quant_model.tflite")
 interpreter.allocate_tensors()
